b8f8ca2573afa1c2504341c59458245c3bb2960a
Marc Schaar <m-schaar@gmx.net>
2025-07-07 18:55:59 +0200

    add database csv file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PseElementModule } from './pse_element/pse_element.module';
 
 @Module({
-  imports: [],
+  imports: [PseElementModule],
   controllers: [AppController],
   providers: [AppService],
 })
