d546052099f55e1177dcc452d2e5a6fec57158a8
Marc Schaar <m-schaar@gmx.net>
2025-07-11 22:14:38 +0200

    feat: allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
